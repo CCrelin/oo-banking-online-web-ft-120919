b7af41a33d583956b617121ca56e333216f13c8a
Cliff Crelin <cliff.crelin@gmail.com>
2020-01-12 04:42:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       @sender.balance = @sender.balance - self.amount
       self.status = "complete"
     else
-      return "Transaction rejected. Please check your account balance." && self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
       
     end
   end 
