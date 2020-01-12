ca778453e089890c71698522f0943ec767b4acb7
Cliff Crelin <cliff.crelin@gmail.com>
2020-01-12 04:38:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 require "pry"
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-
+  attr_accessor :status
+  attr_reader :amount, :sender, :reciever
+  
   def initialize(sender, receiver, amount)
     @sender = sender
-   @receiver = receiver 
+    @receiver = receiver 
     @status = "pending"
     @amount = amount 
   end
